01ec8f471b77ace19322d204485f3c56ae9cd439
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2025-01-11 21:34:07 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import ChaosBlock from "./ChaosBlock";
+import LessonCards from "./LessonCards";
+import ProjectsGallery from "./ProjectsGallery";
+import ContactForm from "./ContactForm";
+import Footer from "./Footer";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <HeroSection />
+      <ChaosBlock />
+      <LessonCards />
+      <ProjectsGallery />
+      <ContactForm />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
